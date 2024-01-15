5b3d9290d216e1f9c357396ac82a06c2a06d168e
Julius Ian Valdez <39887273+juls-ian@users.noreply.github.com>
2024-01-15 22:04:39 +0800

    Version 1.8: Emergency Supply

--- app/src/main/java/com/example/readysilience/EmergencySupplyFrag.java
+++ app/src/main/java/com/example/readysilience/EmergencySupplyFrag.java
@@ -7,15 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EmergencySupplyFrag#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class EmergencySupplyFrag extends Fragment {
 
-
+    GridView gridViewSupplies;
+    ArrayList<Integer> supplyPics;
+    ArrayList<String> supplyNames;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,21 @@ public class EmergencySupplyFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emergency_supply, container, false);
+        View view = inflater.inflate(R.layout.fragment_emergency_supply, container, false);
+
+        gridViewSupplies = view.findViewById(R.id.supplies_grid_view);
+        supplyPics = new ArrayList<>();
+        supplyNames = new ArrayList<>();
+
+        supplyPics.addAll(Arrays.asList(R.drawable.supply_battery, R.drawable.supply_blankets, R.drawable.supply_canned_foods,
+                R.drawable.supply_firstaidkit, R.drawable.supply_flashlight, R.drawable.supply_radio, R.drawable.supply_water,
+                R.drawable.supply_whistle));
+        supplyNames.addAll(Arrays.asList("Batteries", "Blankets", "Canned Goods", "First Aid Kit", "Flashlight",
+                "Radio", "Bottled Water", "Whistle"));
+
+        AdapterGridSupply adapterGridSupply = new AdapterGridSupply(getContext(), supplyPics, supplyNames);
+        gridViewSupplies.setAdapter(adapterGridSupply);
+
+        return view;
     }
 }
