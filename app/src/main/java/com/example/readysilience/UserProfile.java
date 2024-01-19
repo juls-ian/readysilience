99ddafd6242530020e076e6428d61d6ef1e5e221
Julius Ian Valdez <39887273+juls-ian@users.noreply.github.com>
2024-01-19 23:36:16 +0800

    Version 2.2: User Profile

--- app/src/main/java/com/example/readysilience/UserProfile.java
+++ app/src/main/java/com/example/readysilience/UserProfile.java
@@ -1,6 +1,7 @@
 package com.example.readysilience;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class UserProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 }
