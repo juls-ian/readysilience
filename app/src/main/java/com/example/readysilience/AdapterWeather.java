fd0e3ce5552d5d09631fe1a2f98aaeea056d9265
M4NgoJELLY <lincolnmorte008@gmail.com>
2024-02-03 22:04:47 +0800

    version 3.0 backend

--- app/src/main/java/com/example/readysilience/AdapterWeather.java
+++ app/src/main/java/com/example/readysilience/AdapterWeather.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
-import com.example.readysilience.ExpandedViews.WeatherExpandedView;
+//import com.example.readysilience.ExpandedViews.WeatherExpandedView;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -65,8 +65,8 @@ public class AdapterWeather extends RecyclerView.Adapter<AdapterWeather.WeatherV
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, WeatherExpandedView.class);
-                context.startActivity(intent);
+//                Intent intent = new Intent(context, WeatherExpandedView.class);
+//                context.startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/readysilience/ExpandedViews/WeatherExpandedView.java
+++ app/src/main/java/com/example/readysilience/ExpandedViews/WeatherExpandedView.java
@@ -1,28 +1,28 @@
-package com.example.readysilience.ExpandedViews;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-
-import com.example.readysilience.R;
-
-
-public class WeatherExpandedView extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather_expanded_view);
-
-        ImageButton backButton = findViewById(R.id.back_button_weather);
-
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
-}
+//package com.example.readysilience.ExpandedViews;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.ImageButton;
+//
+//import com.example.readysilience.R;
+//
+//
+//public class WeatherExpandedView extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_weather_expanded_view);
+//
+//        ImageButton backButton = findViewById(R.id.back_button_weather);
+//
+//        backButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+//    }
+//}
